fc89095aa822eb18f12db0fb0480f62d5c094728
Lukylix <dev.garcia.lucas@gmail.com>
2023-06-01 16:58:35 +0200

    Fix program exit on tray

--- src/main/index.js
+++ src/main/index.js
@@ -302,12 +302,3 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
